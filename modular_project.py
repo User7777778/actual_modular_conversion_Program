7fdd7f6f5e0a9492036df4d8b01f68250cf02f2c
User7777778 <164092730+User7777778@users.noreply.github.com>
2024-03-25 17:33:34 +1300

    added functions

---
+++ modular_project.py
@@ -0,0 +1,18 @@
+## Conversion Program
+# the program will ask the user for an input and then will convert it into different units
+# the program must print out a menu asking for which conversion they want to do
+# the program must utilise parameters and returns
+# the program should be robust and should not allow invalid input
+
+# YOUR FUNCTION HERE
+def knots_to_kmh(knots):
+    return knots * 1.852
+
+def feet_to_cm(feet):
+    return feet * 30.48
+
+def inches_to_cm(inches):
+    return inches * 2.54
+
+def cups_to_grams(cups):
+    return cups * 128
